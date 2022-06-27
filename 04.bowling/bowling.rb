d9ee3e94409c3301089740045c1dae9c21362ec8
yuji <foxylady0315@icloud.com>
2022-06-28 04:08:11 +0900

    add  bowling 修正3

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -22,16 +22,20 @@ end
 
 point = 0
 frames.each_with_index do |frame, i|
-  point += if frames[i][0] == 10 && i == 9
-             10 + frames[i][1] + frames[i][2]
-           elsif frames[i][0] == 10 && frames[i + 1][0] == 10 && i < 8
-             20 + frames[i + 2][0]
-           elsif frames[i][0] == 10
-             10 + frames[i + 1][0] + frames[i + 1][1]
-           elsif frames[i].sum == 10
-             frames[i].sum + frames[i + 1][0]
-           else
-             frame.sum
-           end
+  if i == 9
+    point += frames[i].sum
+  else
+    unless i == 9
+      point += if i < 8 && frames[i][0] == 10 && frames[i + 1][0] == 10
+                 20 + frames[i + 2][0]
+               elsif frames[i][0] == 10
+                 10 + frames[i + 1][0] + frames[i + 1][1]
+               elsif frames[i].sum == 10
+                 frames[i].sum + frames[i + 1][0]
+               else
+                 frame.sum
+               end
+    end
+  end
 end
 puts point
